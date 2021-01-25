535ef0f5bde9d7a2cb818e3aa159f1a7ab04a898
李佳成 <2713554182@qq.com>
2021-01-25 13:46:24 +0800

    没事

---
+++ fishery-cloud-platform/src/components/ljc/product/product.js
@@ -0,0 +1,15 @@
+module.exports = class {
+    constructor(vue) {
+        this.vue = vue;
+    }
+
+    /* 标签开始 */
+    labels = {
+        id: "加工厂编号",
+        productName: "产品名称",
+        germchitId: "种苗编号",
+        processingFactoryId: "加工厂编号",
+
+    }
+    /* 标签结束 */
+}
