f224b605577bb180f49b376cd728c043d5e87643
李佳成 <2713554182@qq.com>
2021-01-30 11:55:51 +0800

    。。。

--- fishery-cloud-platform/src/components/ljc/craftInput/Table.vue
+++ fishery-cloud-platform/src/components/ljc/craftInput/Table.vue
@@ -102,7 +102,6 @@
       <el-table-column
         prop="inputProduceDate"
         :label="labels.inputProduceDate"
-        width="100px"
         align="center"
       ></el-table-column>
       <el-table-column label="操作" width="120px">
