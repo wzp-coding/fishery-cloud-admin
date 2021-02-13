6e10d00e93f88a9cd2ed953774a87738437ac37e
hwpo <2236277721@qq.com>
2021-02-13 22:17:27 +0800

    权限差后端完善接口，分装分页组件

--- fishery-cloud-platform/src/components/wzp/digital_base/InfoBase.vue
+++ fishery-cloud-platform/src/components/wzp/digital_base/InfoBase.vue
@@ -8,15 +8,15 @@
     <el-row class="baseInformation">
       <p>
         <i class="el-icon-office-building"></i>
-        {{ baseInfoForm.baseName }}
+        {{ baseInfo.baseName }}
       </p>
       <p>
         <i class="el-icon-user"></i>
-        {{ baseInfoForm.createBy }}
+        {{ baseInfo.createBy }}
       </p>
       <p>
         <i class="el-icon-map-location"></i>
-        {{ baseInfoForm.baseAddr }}
+        {{ baseInfo.baseAddr }}
       </p>
       <p>
         <i class="el-icon-pie-chart"></i>
@@ -33,22 +33,7 @@
 export default {
   data() {
     return {
-      baseInfoForm: {
-        baseAddr: "广州市海珠区",
-        baseIntroduction:
-          "仲恺农业工程学院（Zhongkai University of Agriculture and Engineering）简称仲恺，创办于1927年，是一所以伟大的爱国主义者、近代民主革命家廖仲恺先生名字命名，以现代农业科学为特色，农学、工学为优势，农、工、理、经、管、文、艺、法八大学科协调发展的广东省省属应用研究型大学，是教育部本科教学评估优秀学校及全国首批卓越农林人才教育培养计划高校。↵学校办学历史悠久，文脉深厚，坐落在历史文化名城——广州。现有海珠校区、白云校区、番禺教学科研基地，占地面积2264余亩。校园集云山之神秀，汇珠水之灵气，是读书治学的理想地。",
-        baseName: "Super基地",
-        basePic:
-          "http://134.175.208.235/group1/M00/00/28/rBAAD1_YZIyAHq-UAAGH2TCKbN0940.jpg;http://134.175.208.235/group1/M00/00/29/rBAAD1_5Q3eAB1JXAAFNS57b_0o146.jpg;http://134.175.208.235/group1/M00/00/29/rBAAD1_5Q3qAYZdLAACvnk9mlZg029.jpg",
-        basePositionLatitude: "23.08331",
-        basePositionLongitude: "113.3172",
-        createBy: "大老板",
-        createDate: "2020-08-06 23:25:00",
-        funds: 10000,
-        id: "1248910886228332544",
-        registNumber: "10101010101010",
-        scope: "虾类养殖",
-      },
+      baseInfo: {},
       pondInfo: {
         volume: 0,
         pondNum: 0,
